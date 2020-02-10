4956408659aba9704c9a48c12effaa888a3e48c5
Alexander Craig <alexandercraig@ORRHS.ORG>
2020-02-10 12:32:43 -0500

    Made Clicker Game

--- Lesson 1/App.js
+++ Lesson 1/App.js
@@ -1,8 +1,47 @@
-function setup() {
-    createCanvas(windowWidth, windowHeight)
-    background('beige')
-}
+// function setup() {
+//     createCanvas(windowWidth, 10);
+//     createElement('h1', 'My lucky number is...');
+//     //console.log(numText) // Find out what is inside of this newly created numText objects
+//   }
 
-function draw(){
-    createP('This was created by William Michael Stark')
-}
+// function mousePressed(){
+//     let generate_number = createP('my lucky number is ' + random(0,10))
+// }
+
+// function draw(){
+//     background(0)
+// }
+
+
+// BUTTON CLICK LISTENER
+// SAY HELLO
+
+// let button;
+// let greeting;
+
+// function setup() {
+//     createCanvas(windowWidth, windowHeight)
+//     button = createButton('Try Me');
+//     button.position(width / 2, height / 2)
+//     button.mousePressed(greet)
+//     greeting = createElement('h2', 'Say Hello!')
+//     greeting.position(width / 2, height / 3)
+//     textAlign(CENTER)
+//     textSize(50)
+      
+// }
+
+// function greet() {
+//     greeting.html('HELLO!')
+
+//     for (let i = 0; i < 200; i++) {
+//         push()
+//         fill(random(255),random(255),random(255));
+//         translate(random(width), random(height))
+//         rotate(random(2 * PI));
+//         text('HELLO', 0, 0);
+//         pop();
+
+//     }
+// }
+
