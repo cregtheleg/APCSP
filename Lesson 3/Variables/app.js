4d5a42fba7be2ad4b66b822ced78a5915f52528d
Alexander Craig <alexandercraig@ORRHS.ORG>
2020-02-12 10:10:34 -0500

    Lesson 3

---
+++ Lesson 3/Variables/app.js
@@ -0,0 +1,13 @@
+
+function setup() {
+    a = random(1,6)
+    b = random(1,6)
+    t = a + b
+    console.log('Rolling Dice')
+    console.log('You rolled a: ' + a)
+    console.log('and a: ' + b)
+    console.log('For a t of: ' + t)
+}
+
+
+
